fd2ec653ede62e9a1cb091654ce812dc2b248a88
Harsh Nagarkar <hknagark@go.olemiss.edu>
2020-04-01 01:59:53 -0500

    the database scheme change and upload image feature added

--- UserAPI/routes/index.js
+++ UserAPI/routes/index.js
@@ -7,7 +7,10 @@ var jwt = require("jsonwebtoken");
 var cookieParser = require("cookie-parser");
 var mysql = require("mysql");
 const util = require("util");
+const multer = require("multer");
 var app = express();
+
+
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -16,6 +19,9 @@ app.use(
     extended: true
   })
 );
+
+
+//-----------------------------------------------------------------JWT
 
 var publicKEY = fs.readFileSync(
   path.join(__dirname, "../keys/public.key"),
@@ -41,6 +47,47 @@ var options = {
   }
 };
 
+//----------------------------------------------fileupload-------------------------------------------------------------------------------
+var storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, path.join(__dirname, "../data")); //here we specify the destination. in this case i specified the current directory
+  },
+  filename: function(req, file, cb) {
+    console.log(file); //log the file object info in console
+    cb(null, file.fieldname + '-' + Date.now() + '.' + file.originalname.split('.')[file.originalname.split('.').length -1]);//here we specify the file saving name. in this case. 
+//i specified the original file name .you can modify this name to anything you want
+  }
+});
+
+var uploadDisk = multer({ 
+  storage: storage,
+  fileFilter: function (req, file, callback) {
+    var ext = path.extname(file.originalname);
+    if(ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
+        return callback(new Error('Only images are allowed'))
+    }
+    callback(null, true)
+    },
+    limits:{
+        fileSize: 1024 * 1024
+    }
+});
+
+//----------------------------------database---------------------------------------------------------------
+
+var writeconnection = mysql.createConnection({
+  host: "mariadb-1-mariadb.mariadb.svc.cluster.local",
+  user: "Suser",
+  password: '<mwxe2H/3f8Kyb$N',
+  database: "stockUser",
+})
+
+writeconnection.connect(function (err) {
+  if (err) {
+    console.log('error when connecting to db:', err);
+  }
+})
+
 var readconnection = mysql.createConnection({
   host: "mariadb-1-mariadb-secondary.mariadb.svc.cluster.local",
   user: "Suser",
@@ -54,29 +101,53 @@ readconnection.connect(function(err) {
   }
 });
 
+//----------------------------------------------------------------------------------------------------------------------
+function  authcheck(req,res,next){
+  var token = req.cookies.token;
+  console.log(req.body.image)
+  jwt.verify(token, publicKEY, verifyOptionsJWT, function(err, decoded) {
+    if (err) {
+      res.sendStatus(401)
+    } else {
+      next()
+    }
+  });
+ };
+
 /* GET home page. */
 router.get("/user/", function(req, res, next) {
   res.render("index", { title: "Express" });
 });
 
 router.get("/user/data", function(req, res, next) {
-  console.log(req.query);
   var token = req.cookies.token;
   jwt.verify(token, publicKEY, verifyOptionsJWT, function(err, decoded) {
     if (err) {
       res.sendStatus(401);
     } else {
       readconnection.query(
-        "Select * from User where email='" + decoded.email + "'",
+        "Select reputation,correctprediction,prediction,profile from User where email='" + decoded.email + "'",
         function(err, result, fields) {
           if (err) res.sendStatus(500);
           console.log(decoded.email, " ", result);
-          res.send(result);
+          res.json(result)
         }
       );
     }
   });
 });
+
+router.post("/user/uploadimage",authcheck,uploadDisk.single("image"),function(req,res,next){
+  writeconnection.query(
+    "INSERT INTO \`User\` (profileimage) VALUES ("+req.filename+")  ON DUPLICATE KEY UPDATE profilename='" + req.filename + "'",
+    function(err, result, fields) {
+      if (err) res.sendStatus(500);
+      console.log(decoded.email, " ", result);
+      console.log(req.filename)
+      res.sendStatus(200)
+    }
+  );  
+})
 
 router.get("/user/image/:name", function(req, res, next) {
   console.log(req.params);
